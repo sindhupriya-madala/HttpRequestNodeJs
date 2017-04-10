7e0ed94cf77bf6b7fd0830e3c69c2fa819620923
sindhupriya-madala <madala.sindhu@gmail.com>
2017-04-10 19:24:02 +0000

    getAndPrintHtmlInputURL.js added.takes URL as an Argument so our function is reusable

--- getAndPrintHtmlInputURL.js
+++ getAndPrintHtmlInputURL.js
@@ -2,20 +2,22 @@ function getAndPrintHTML (options) {
 
   const https = require("https");
   let responceString = '';
-
   https.get(options, (response) => {
     response.on('data', (chunk) => {
+      //Adding chunks of data to a string variable.until response ends.
       responceString += chunk;
     });
+    //following event occurs when our response ends.
     response.on('end', () => {
           console.log("responce string is : ",responceString);
     });
   });
 
 }
-
+// preparing request URL object.
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step3.html'
 };
+
 getAndPrintHTML(requestOptions);
